0027e822e9ea043f3dcdbbc81f9819e88c326802
kath0809 <kath014@student.kristiania.no>
2024-09-12 19:20:01 +0200

    Initial Commit

---
+++ SightseeingLocationsApp/SightseeingLocationsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SightseeingLocationsAppApp.swift
+//  SightseeingLocationsApp
+//
+//  Created by Karima Thingvold on 12/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct SightseeingLocationsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
