572f80319b0569cb0c77cb8669af439a95235952
kath0809 <kath014@student.kristiania.no>
2024-09-12 21:20:34 +0200

    Added the map

--- SightseeingLocationsApp/ViewModels/LocationsViewModel.swift
+++ SightseeingLocationsApp/ViewModels/LocationsViewModel.swift
@@ -6,14 +6,37 @@
 //
 
 import Foundation
+import MapKit
+import _MapKit_SwiftUI
 
 class LocationsViewModel: ObservableObject {
     
+    /// All loaded locations
     @Published var locations: [Location]
+    
+    // Maps current location
+    @Published var mapLocation: Location {
+        didSet {
+            updateMapRegion(location: mapLocation)
+        }
+    }
+    
+    @Published var cameraPosition: MapCameraPosition = MapCameraPosition.region(MKCoordinateRegion())
+    let mapSpan = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
     
     init() {
         let locations = LocationsDataService.locations
         self.locations = locations
+        self.mapLocation = locations.first!
+        self.updateMapRegion(location: locations.first!)
     }
     
-}
+    private func updateMapRegion(location: Location) {
+        let region = MKCoordinateRegion(
+                    center: location.coordinates,
+                    span: mapSpan)
+                
+                // Update to MapCameraPosition
+                cameraPosition = .region(region)
+        }
+    }
--- SightseeingLocationsApp/Views/LocationsView.swift
+++ SightseeingLocationsApp/Views/LocationsView.swift
@@ -6,20 +6,24 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct LocationsView: View {
     
     //@State private var vm = LocationsViewModel()
     @EnvironmentObject private var vm: LocationsViewModel
     
+//    @State private var cameraPosition = MapCameraPosition.region(MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 41.8902, longitude: 12.4922), span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)))
+    
     var body: some View {
-        List {
-            ForEach(vm.locations) {
-                Text($0.name)
-            }
+        ZStack {
+            Map(position: $vm.cameraPosition)
+                .ignoresSafeArea()
         }
     }
 }
+
+
 
 #Preview {
     LocationsView()
